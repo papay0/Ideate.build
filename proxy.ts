3bddad9085ec1c17db916e51062409ee075dacd1
Arthur Papailhau <papailhau@gmail.com>
2025-12-24 00:51:48 +0100

    Allow anonymous access to OG route

--- proxy.ts
+++ proxy.ts
@@ -5,6 +5,7 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/webhook(.*)',
+  '/api/og(.*)',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
